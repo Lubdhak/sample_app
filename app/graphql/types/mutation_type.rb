d8edd85276ba825a706e545c45505ce9883b2172
Lubdhak Mahapatra <lubdhakmahapatra@LMAHAPATRA-MBP.local>
2019-09-05 13:43:59 +0530

    mutation_query_here

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
     # TODO: remove me
-    field :update_table_entry, mutation: Mutations::UpdateTableEntry 
+    field :update_table_entry, mutation: Mutations::UpdateTableEntry,
+    description: "mutation_1"
   end
 end
