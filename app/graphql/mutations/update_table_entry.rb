69a923f0184fcd92ee47b3d45c9d28190f9b0003
Lubdhak Mahapatra <lubdhakmahapatra@LMAHAPATRA-MBP.local>
2019-09-05 19:08:43 +0530

    d2

--- app/graphql/mutations/update_table_entry.rb
+++ app/graphql/mutations/update_table_entry.rb
@@ -3,7 +3,6 @@ module Mutations
         null true 
         argument :id, ID, required: true, camelize: false
         argument :title, String, required: true, camelize: false
-      
         field :table_entity, Types::MyTableType, null: true, camelize: false
         
         def resolve(**inputs)
