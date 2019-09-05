1b51f400a9093b8d63aace2bc7fc2d4b8b9e6a72
Lubdhak Mahapatra <lubdhakmahapatra@LMAHAPATRA-MBP.local>
2019-09-05 12:43:50 +0530

    all queries here

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,16 +4,18 @@ module Types
     # They will be entry points for queries on your schema.
     # TODO: remove me
     field :test_field, [String], null: false, camelize: false,
-    description: "An example field added by the generator"
+    description: "query_1"
     
-    field :table_entities, [Types::MyTableType], null: false, camelize: false
+    field :table_entities, [Types::MyTableType], null: false, camelize: false,
+    description: "query_2"
 
-    field :table_entity, Types::MyTableType, null: false, camelize: false do
+    field :table_entity, Types::MyTableType, null: false, camelize: false,
+    description: "query_3" do
       argument :id, ID, required: true
     end
 
     field :test_fieldw, String, null: false, camelize: false,
-    description: "An example field added by the generator"
+    description: "query_4"
     
     def table_entity(id: )
       Mytable.find(id)
