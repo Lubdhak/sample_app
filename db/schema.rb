1c59de30a3c3f607636829e5f8f109d23ea50cc7
Lubdhak Mahapatra <lubdhakmahapatra@LMAHAPATRA-MBP.local>
2019-11-22 10:57:26 +0530

    rename a table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_29_061251) do
+ActiveRecord::Schema.define(version: 2019_11_22_052352) do
 
-  create_table "mytables", force: :cascade do |t|
+  create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
